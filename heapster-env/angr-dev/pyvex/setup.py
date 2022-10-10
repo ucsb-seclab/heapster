6bcb2cba8321eb4d18c2b92be6de415335f79865
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-10 01:20:30 +0000

    Adding tarfile member sanitization to extractall()

--- heapster-env/angr-dev/pyvex/setup.py
+++ heapster-env/angr-dev/pyvex/setup.py
@@ -66,7 +66,26 @@ if not os.path.exists(VEX_PATH):
     with open('vex-master.tar.gz', 'wb') as v:
         v.write(urlopen(VEX_URL).read())
     with tarfile.open('vex-master.tar.gz') as tar:
-        tar.extractall()
+        def is_within_directory(directory, target):
+            
+            abs_directory = os.path.abspath(directory)
+            abs_target = os.path.abspath(target)
+        
+            prefix = os.path.commonprefix([abs_directory, abs_target])
+            
+            return prefix == abs_directory
+        
+        def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+        
+            for member in tar.getmembers():
+                member_path = os.path.join(path, member.name)
+                if not is_within_directory(path, member_path):
+                    raise Exception("Attempted Path Traversal in Tar File")
+        
+            tar.extractall(path, members, numeric_owner=numeric_owner) 
+            
+        
+        safe_extract(tar)
 
 def _build_vex():
     e = os.environ.copy()
